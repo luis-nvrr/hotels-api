68f198fcae71bce76a1e5225a6089cdbbad665f3
Luis Navarro <luisfnvrr@gmail.com>
2022-01-27 08:54:39 -0300

    initial commit

---
+++ src/main/java/meli/challenge/quality/QualityApplication.java
@@ -0,0 +1,13 @@
+package meli.challenge.quality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/meli/challenge/quality/QualityApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.challenge.quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
